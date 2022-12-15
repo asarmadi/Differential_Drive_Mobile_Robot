f8b90832aeeb7e1ed32ba90d1d960529d09cc329
asarmadi <a.sarmadi@yahoo.com>
2022-12-15 01:37:26 -0500

    comments added to robot

--- robot.py
+++ robot.py
@@ -17,15 +17,35 @@ class Robot:
                          [((self.R**2)*(self.m*self.L**2-self.I))/(4*self.L**2),self.I_w+((self.R**2)*(self.m*self.L**2+self.I))/(4*self.L**2)] ])
     self.B   = np.array([[1,0], [0,1]])
 
- def forward_kinematic(self, theta, phi):
+ def forward_kinematic(self, theta, d_phi):
+     '''
+     Calculates the forward kinematics of the robot
+
+     Args:
+         theta: The orientation of the robot
+         d_phi: The vector of wheels angular velocities (dot [phi_r; phi_l])
+
+     Returns:
+         Pose vector as dot [x_a; y_a; theta; phi_r; phi_l]
+     '''
      R = 0.5*np.array([[self.R*cos(theta), self.R*cos(theta)], 
                        [self.R*sin(theta), self.R*sin(theta)], 
                        [self.R/self.L, -self.R/self.L], 
                        [2, 0],
                        [0, 2] ])
-     return R.dot(phi)
+     return R.dot(d_phi)
 
  def forward_dynamics(self, eta, theta, tau):
+     '''
+     Calculates the forward kinematics of the robot
+
+     Args:
+         theta: The orientation of the robot
+         d_phi: The vector of wheels angular velocities (dot [phi_r; phi_l])
+
+     Returns:
+         Pose vector as dot [x_a; y_a; theta; phi_r; phi_l]
+     '''
      V = np.array([[0, (self.R**2*self.m_c*self.d*dtheta)/(2*self.L)], 
                   [-(self.R**2*self.m_c*self.d*dtheta)/(2*self.L),0] ])
      return np.linalg.inv(self.M)@(self.B@tau - V@eta)
